2a7cee5d0bb3bd781ffc735378de055673dafdb0
Freddie Brewin <freddiebrewin@live.com>
2021-08-24 16:59:52 +0100

    Setting up database connection

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-'use strict';
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
 
-const express = require(`express`);
-const mongoose = require(`mongoose`);
-
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const app = express();
 
+app.use(logger("dev"));
+
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(`public`));
+app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/budget`, {
     useNewUrlParser: true,
@@ -18,8 +21,9 @@ mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/budget`, {
     useUnifiedTopology: true
 });
 
-
 // routes
-app.use(require(`./routes/api.js`));
+app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
